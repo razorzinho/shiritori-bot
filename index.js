acf4aa06dc95235cf21cbf3be638a58d7fe21ec4
r47orr <razoroller529@gmail.com>
2021-09-26 01:23:19 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client } = require('discord-js');
+var json = require('./config.json');
+require('dotenv').config();
+
+const prefix = json.configs.prefix;
+const intents = json.configs.intents;
+
+const client = new Client({intents = intents});
+
+client.on('message', async (message) => {
+    if (message.author.client) return;
+    if (message.channel.type === 'dm') return;
+    if (message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const commandName = args.shift().toLowerCase();
+
+        const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        if(!command) return;
+    }
+});
+
+client.once('ready', () => {
+    console.log('Estou on-line!');
+})
+
+client.login(process.env.TOKEN)
